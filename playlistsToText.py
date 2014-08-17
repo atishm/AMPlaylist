87baac4946c15fff79fa88ed982982dc7e5c6bad
Atish Mehta <atishm@fb.com>
2014-08-17 11:40:06 -0700

    walkin da node

--- playlistsToText.py
+++ playlistsToText.py
@@ -9,6 +9,8 @@ else:
   directory = sys.argv[1]
   print "Using directory %s" % directory
 
+# The traktor playlist folder name
+FOLDER_NAME = "2014 BM"
 
 def main():
   for subdir, dirs, files in os.walk(directory):
@@ -28,6 +30,12 @@ def printPlaylist(fileName):
   try:
     tree = ET.parse(fileName)
     root = tree.getroot()
+    
+    desiredFolderNode = findDesiredFolderNode(root)
+    for playlist in desiredFolderNode.iter('NODE'):
+      nodeType = safeParse(playlist, 'TYPE')
+      if (nodeType == "PLAYLIST"):
+        parsePlaylist(playlist)
   
     collection = tree.find('COLLECTION')
     for entry in collection.findall('ENTRY'):
@@ -50,11 +58,28 @@ def printPlaylist(fileName):
       tempo = entry.find('TEMPO')
       bpm = safeParse(tempo, 'BPM')
     
-      print "%s \t %s \t %s \t %s \t %s \t %s" %  (title, time, artist, bpm, comment, key)
+#print "%s \t %s \t %s \t %s \t %s \t %s" %  (title, time, artist, bpm, comment, key)
 
 
   except Exception, message:
     print "%s %s" % (Exception, message)
+
+
+def parsePlaylist(nodePlaylist):
+  playlistName = safeParse(nodePlaylist, 'NAME')
+  for primaryKey in nodePlaylist.iter('PRIMARYKEY'):
+    keyTpe = safeParse(primaryKey, 'TYPE')
+    key = safeParse(primaryKey, 'KEY')
+    print key
+
+
+def findDesiredFolderNode(root):
+  for node in root.iter('NODE'):
+    nodeType = safeParse(node, 'TYPE')
+    nodeName = safeParse(node, 'NAME')
+    if (nodeType == "FOLDER" and nodeName == "2014 BM"):
+      return node
+  return None
 
 def safeParse(element, attributeName):
   attribute = element.get(attributeName)
