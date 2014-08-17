2732eeee3b78abaeac130560ce3654d2b30fe0f1
Atish Mehta <atishm@fb.com>
2014-08-17 12:21:42 -0700

    seems to parse the entire library

--- playlistsToText.py
+++ playlistsToText.py
@@ -3,7 +3,7 @@ from os.path import join, abspath
 import xml.etree.ElementTree as ET
 
 if len(sys.argv) != 2:
-  print "Usage: playlistToText.py [path to nmls]"
+  print "Usage: playlistToText.py [path to collection file directory]"
   exit(1)
 else:
   directory = sys.argv[1]
@@ -11,7 +11,6 @@ else:
 
 # The traktor playlist folder name
 FOLDER_NAME = "2014 BM"
-
 def main():
   for subdir, dirs, files in os.walk(directory):
     for file in files:
@@ -19,59 +18,56 @@ def main():
       try:
         filetype = str(filename[-3:]).upper()
         if str(filetype) == str('NML'):
-          printPlaylist(filename)
+          parseCollection(filename)
     
       except Exception, message:
           print "Problem:", message
 
-def printPlaylist(fileName):
+trackKeysInDesiredPlaylists = {}
+allTracks = {}
+
+def parseCollection(fileName):
 #iconv -f UTF-8 -t ISO-8859-15 in.txt > out.txt
-  print "parsing ", fileName
+  print "Parsing %s \n " % fileName
   try:
     tree = ET.parse(fileName)
     root = tree.getroot()
     
+    parseAllTracks(root)
+    
+    writePath = "%s/playlists.txt" % directory
+    writeFile = open(writePath, 'w')
+
     desiredFolderNode = findDesiredFolderNode(root)
     for playlist in desiredFolderNode.iter('NODE'):
       nodeType = safeParse(playlist, 'TYPE')
       if (nodeType == "PLAYLIST"):
-        parsePlaylist(playlist)
-  
-    collection = tree.find('COLLECTION')
-    for entry in collection.findall('ENTRY'):
-      title = safeParse(entry, 'TITLE')
-      artist = safeParse(entry, 'ARTIST')
-      
+        parsedPlaylist = parsePlaylist(playlist)
+        print parsedPlaylist
+        writeFile.write("\n")
+        writeFile.write(parsedPlaylist)
 
-      #info
-      info = entry.find('INFO')
-      comment = safeParse(info, 'COMMENT')
-      playtime = int(safeParse(info, 'PLAYTIME'))
-      minutes = playtime / 60
-      seconds = playtime & 60
-      if (seconds < 60):
-        seconds = "0%s" % seconds
-      time = "%s:%s" % (minutes, seconds)
-      key = safeParse(info, 'KEY')
-      
-      #tempo
-      tempo = entry.find('TEMPO')
-      bpm = safeParse(tempo, 'BPM')
-    
-#print "%s \t %s \t %s \t %s \t %s \t %s" %  (title, time, artist, bpm, comment, key)
-
+    writeFile.close()
 
   except Exception, message:
-    print "%s %s" % (Exception, message)
+    print "Top level exception: %s %s" % (Exception, message)
 
 
 def parsePlaylist(nodePlaylist):
   playlistName = safeParse(nodePlaylist, 'NAME')
+  returnString = playlistName
+  print "parsing playlist: ", playlistName
   for primaryKey in nodePlaylist.iter('PRIMARYKEY'):
-    keyTpe = safeParse(primaryKey, 'TYPE')
-    key = safeParse(primaryKey, 'KEY')
-    print key
-
+    try:
+      keyType = safeParse(primaryKey, 'TYPE')
+      key = safeParse(primaryKey, 'KEY')
+      if(key in allTracks):
+        returnString = "%s\n%s" % (returnString, allTracks[key])
+      else:
+        print "key was found in playlist, but not master collection: ", key
+    except Exception, message:
+      print "Error parsing playlist key: %s" % (message)
+  return returnString
 
 def findDesiredFolderNode(root):
   for node in root.iter('NODE'):
@@ -82,10 +78,45 @@ def findDesiredFolderNode(root):
   return None
 
 def safeParse(element, attributeName):
-  attribute = element.get(attributeName)
-  if attribute:
-    return attribute.encode('utf-8')
-  else:
-    return ""
+  if element is not None:
+    attribute = element.get(attributeName)
+    if attribute:
+      return attribute.encode('utf-8')
+  return ""
+
+def parseAllTracks(root):
+  try:
+    collection = root.find('COLLECTION')
+    for entry in collection.findall('ENTRY'):
+      #location
+      location = entry.find('LOCATION')
+      dir = safeParse(location, 'DIR')
+      file = safeParse(location, 'FILE')
+      volume = safeParse(location, 'VOLUME')
+      primaryKey = "%s%s%s" % (volume, dir, file)
+      
+      #print primaryKey
+      title = safeParse(entry, 'TITLE')
+      artist = safeParse(entry, 'ARTIST')
+    
+      #info
+      info = entry.find('INFO')
+      comment = safeParse(info, 'COMMENT')
+      playtime = int(safeParse(info, 'PLAYTIME'))
+      minutes = playtime / 60
+      seconds = playtime & 60
+      if (seconds < 60):
+        seconds = "0%s" % seconds
+        time = "%s:%s" % (minutes, seconds)
+      key = safeParse(info, 'KEY')
+      
+      #tempo
+      tempo = entry.find('TEMPO')
+      bpm = safeParse(tempo, 'BPM')
+
+      printString = "%s \t %s \t %s \t %s \t %s \t %s" %  (title, time, artist, bpm, comment, key)
+      allTracks[primaryKey] = printString
+  except Exception, message:
+    print "Error parsing track: %s %s" % (Exception, message)
 
 main()
